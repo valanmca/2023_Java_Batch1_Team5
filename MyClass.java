6046805fd872c7ccd2cdeccaac8d1f4b895c00c2
Shaji Ram Prakash Balasubramaniyan <shaji.ba@itp.objectfrontier.com>
2023-08-21 16:50:13 +0530

    commitjava1

--- MyClass.java
+++ MyClass.java
@@ -1,10 +1,20 @@
 <<<<<<< HEAD
 class Myclass1 {
+	//variables
+	int a,b;
+	//constructers
+	Myclass1(int a, int b){
+		this.a = a;
+		this.b = b;
+	}
+	//methods
 void addition(){
-
+addition = a+b;
+System.out.println("Addition of a+b is " +addition);
 }
 void subraction(){
-
+subtraction = a-b;
+System.out.println("Addition of a+b is " +subtraction);
 }
 void multiplication(){
 
@@ -16,6 +26,6 @@ void sum(){
 
 }
 void avg(){
-	
+
 }
 }
