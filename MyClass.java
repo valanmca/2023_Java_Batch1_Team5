686b1483d455b8167f2eed637d85869399505247
Sarabudheen Amirudeen <sarabudheen.a@itp.objectfrontier.com>
2023-08-21 16:31:58 +0530

    Sara's Methods are added.

--- MyClass.java
+++ MyClass.java
@@ -7,4 +7,13 @@ My method2()
 {
     
 }
+Sara_Method1(){
+    System.out.println("My Method 1");
+}
+Sara_Method2(){
+    System.out.println("My Method 2");
+}
+}
+public static void main(String arg[]){
+    
 }
