b9fc990bcfd8eefa6a7ad7f03265fab8c1f9d9c6
Swetha Sadhasivam <swetha.sadhasivam@itp.objectfrontier.com>
2023-08-21 16:57:47 +0530

    commited by swetha

--- MyClass.java
+++ MyClass.java
@@ -1,21 +1,29 @@
 <<<<<<< HEAD
 class Myclass1 {
-void addition(){
-
-}
+    int a,b
+    Myclass1(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+//void addition(){
+ 
+//}
 void subraction(){
-
+ int sub=a-b;
+ System.out.println("The subraction of a and b is "+ sub);
 }
 void multiplication(){
-
+int mul=a*b;
+System.out.println("The division of a nad b is "+ mul)
 }
-void division(){
+//void division(){
 
-}
-void sum(){
+//}
+//void sum(){
 
-}
-void avg(){
+//}
+//void avg(){
 	
+//}
 }
-}
+
